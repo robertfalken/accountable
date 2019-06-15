1a53440ea81a34a4ca42aa751829d8a4652316f6
Robert Falkén <f@lken.se>
2019-06-15 11:08:37 +0200

    Read db info from env

--- config/test.exs
+++ config/test.exs
@@ -3,10 +3,10 @@ use Mix.Config
 config :accountable, :ecto_repo, Accountable.TestRepo
 
 config :accountable, Accountable.TestRepo,
-  username: "postgres",
-  password: "postgres",
-  database: "accountable_test",
-  hostname: "localhost",
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  database: System.get_env("POSTGRES_DB"),
+  hostname: System.get_env("POSTGRES_HOST"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :accountable, ecto_repos: [Accountable.TestRepo]
