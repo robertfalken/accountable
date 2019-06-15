2e8b49113798fa98e9e712fb4753ba5e494dbcd7
Robert Falkén <f@lken.se>
2019-06-15 11:44:58 +0200

    Circle CI config

--- config/test.exs
+++ config/test.exs
@@ -3,10 +3,7 @@ use Mix.Config
 config :accountable, :ecto_repo, Accountable.TestRepo
 
 config :accountable, Accountable.TestRepo,
-  username: System.get_env("POSTGRES_USER"),
-  password: System.get_env("POSTGRES_PASSWORD"),
-  database: System.get_env("POSTGRES_DB"),
-  hostname: System.get_env("POSTGRES_HOST"),
+  url: System.get_env("POSTGRES_URL"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :accountable, ecto_repos: [Accountable.TestRepo]
