62e72e0f6d447432868c50fe7889043b52fc7493
Robert Falkén <f@lken.se>
2019-10-16 18:40:54 +0200

    Add reset_password feature

--- lib/accountable.ex
+++ lib/accountable.ex
@@ -18,6 +18,8 @@ defmodule Accountable do
 
   def user_by_id(id), do: repo.get(user_schema, id)
 
+  def user_by_email(email), do: repo.get_by(user_schema(), email: email)
+
   def create_user(attributes) do
     attributes
     |> user_changeset()
@@ -32,7 +34,7 @@ defmodule Accountable do
   @spec token_for_user(struct, String.t()) :: {atom, String.t()}
   def token_for_user(user, type \\ "access") do
     with true <- Enum.member?(@supported_tokens, type),
-         {:ok, token, _claims} <- claims(user) do
+         {:ok, token, _claims} <- claims(user, type) do
       {:ok, token}
     else
       _ ->
@@ -41,14 +43,21 @@ defmodule Accountable do
   end
 
   @spec user_by_token(String.t()) :: {:ok, struct} | {:error, any}
-  def user_by_token(token) do
-    case Accountable.Guardian.resource_from_token(token, %{"typ" => "access"}) do
+  def user_by_token(token, type \\ "access") do
+    case Accountable.Guardian.resource_from_token(token, %{"typ" => type}) do
       {:ok, user, _claims} ->
         {:ok, user}
 
       err ->
         err
     end
+  end
+
+  def reset_password(token, new_password) do
+    {:ok, user} = user_by_token(token, "reset_password")
+    attributes = %{password: new_password} |> put_password_hash()
+    changeset = user_schema.changeset(user, attributes)
+    repo.update!(changeset)
   end
 
   def put_password_hash(%{password: password} = attrs) do
@@ -60,16 +69,16 @@ defmodule Accountable do
   def repo, do: Application.get_env(:accountable, :ecto_repo)
   def user_schema, do: Application.get_env(:accountable, :user_schema, Accountable.User)
 
-  def claims(user) do
-    claims_fn = Application.get_env(:accountable, :claims, &default_claims/1)
-    claims_fn.(user)
+  def claims(user, type \\ "access") do
+    claims_fn = Application.get_env(:accountable, :claims, &default_claims/2)
+    claims_fn.(user, type)
   end
 
-  def default_claims(user) do
+  def default_claims(user, type \\ "access") do
     Accountable.Guardian.encode_and_sign(
       user,
       %{
-        "typ" => "access"
+        "typ" => type
       }
     )
   end
